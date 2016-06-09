69a7f66863ceadd2a16f37c22d8a52092214d396
stanisger <gersigner@hotmail.com>
2016-06-09 17:03:28 -0500

    integración plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,57 @@
     $(".drop").click(function (){
     	$(this).parent('li').children(".dropdown-menu").toggleClass('show');
     });
+
+
+
+
+
+//$('.button-collapse').sideNav('hide');
+  $(".drop").click(function () {
+        $( this ).children(".deg").toggleClass("rotate180");
+        //$( this ).toggleClass( "highlight" );        
+  });
+
+
+ 
+var step = 25;
+var scrolling = false;
+
+// Wire up events for the 'scrollUp' link:
+$("#scrollUp").bind("click", function(event) {
+    event.preventDefault();
+    // Animates the scrollTop property by the specified
+    // step.
+    $("#content").animate({
+        scrollTop: "-=" + step + "px"
+    });
+}).bind("mouseover", function(event) {
+    scrolling = true;
+    scrollContent("up");
+}).bind("mouseout", function(event) {
+    scrolling = false;
+});
+
+
+$("#scrollDown").bind("click", function(event) {
+    event.preventDefault();
+    $("#content").animate({
+        scrollTop: "+=" + step + "px"
+    });
+}).bind("mouseover", function(event) {
+    scrolling = true;
+    scrollContent("down");
+}).bind("mouseout", function(event) {
+    scrolling = false;
+});
+
+function scrollContent(direction) {
+    var amount = (direction === "up" ? "-=1px" : "+=1px");
+    $("#content").animate({
+        scrollTop: amount
+    }, 1, function() {
+        if (scrolling) {
+            scrollContent(direction);
+        }
+    });
+}
